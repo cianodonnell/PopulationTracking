e074b3d6c596c94e3e1ebd50a4f0f7779b017a5f
Cian O'Donnell <cian.odonnell@bristol.ac.uk>
2020-12-09 10:25:31 +0000

    updated stir.binom to return log of result too

--- stir_binom.m
+++ stir_binom.m
@@ -1,7 +1,11 @@
-function [c] = stir_binom(n,k);
+function [c,logc] = stir_binom(n,k);
 % STIR_BINOM(N,K) calculates the binomial coeffcient (n-choose-k) using
 % Stirling's approximation n! = n*log(n)- n. Actually uses extra terms in
 % the approximation so its n! = (n + 1/2)*log(n) - n + (1/2)*log(2*pi)
+%
+% Returns:
+% c = the actual number
+% logc = the log of the actual number (useful if c is too big to store directly, sometimes returns as Inf)
 
 logc = (n+ 1/2)*log(n) - (k + 1/2)*log(k) - (n - k + 1/2)*log(n-k) - (1/2)*log(2*pi);
 c = round(exp(logc));
